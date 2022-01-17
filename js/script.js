833fd526061b27326ff95073ea11b545ef425546
Michaelh0 <76009001+Michaelh0@users.noreply.github.com>
2022-01-17 12:56:32 -0800

    adding progress bar

--- js/script.js
+++ js/script.js
@@ -115,3 +115,21 @@ function clean_up_word(word){
 
 }
 
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
